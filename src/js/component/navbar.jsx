4e6f2e58bd37e160ff3eeeafae3e73d74e2785f0
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-08-17 12:23:06 +0000

    Fix navbar dark theme

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 
-// TODO:
-// - Align nav items to the right in lg size.
-// - Make hamburger white.
 export default function Navbar() {
     return (
-    <nav className="navbar navbar-expand-lg bg-dark">
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand mb-0 text-white h1" href="#">Start Bootstrap</a>
+            <a className="navbar-brand mb-0 h1" href="#">Start Bootstrap</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-supported-content" aria-controls="navbar-supported-content" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbar-supported-content">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
-                        <a className="nav-link text-white active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">About</a>
+                        <a className="nav-link" href="#">About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">Services</a>
+                        <a className="nav-link" href="#">Services</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="#">Contact</a>
+                        <a className="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
             </div>
